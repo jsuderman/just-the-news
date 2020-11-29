01d4a75f9263b59e4cb28d531ffdc8e905362cd7
Justin Suderman <jsuderman1@gmail.com>
2020-11-28 20:52:03 -0800

    added backend server to connect with frontend

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dbUserJAS:NIKEhoops123!@jas.stgnf.mongodb.net/auth?retryWrites=true&w=majority"
-};
--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+//import routes
+const authRoutes = require('./backend/routes/auth');
+const { db } = require('./backend/models/User');
+//app
 const app = express();
-
-
-//middleware
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
-app.use(bodyParser.json());
-
-//DB config
-const db = require("./config/keys").mongoURI;
-
-//connect to MongoDb
+// db
 mongoose
-    .connect(
-        db,
-        { useNewUrlParser: true }
-    )
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
-
-const port = process.env.PORT || 5000;  
-
-app.listen(port, () => console.log(`Server up and running on port ${port}!`));
+  .connect(process.env.DATABASE,{
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+   })
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+//routes middleware
+app.use('/api', authRoutes);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+});
